330bebb8fc4b7ccf4fcf8b6aa20e71ec1c8e5813
rsshonjoydas <rsshonjoydas@gmail.com>
2022-01-11 18:17:15 +0600

    Tailwind Configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: {
+          300: "#6c5ce7"
+        },
+        secondaryColor: {
+          600: "#a29bfe"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
